fb364a01dc2777524c56ae1f49ae547cb729e2fe
Anand Mallik <ccanandmallik@gmail.com>
2022-10-08 00:18:15 -0400

    its true

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -6,7 +6,7 @@ const Games: React.FC = () => (
     {/*  TODO: make this cards with MUI stuff etc */}
     <ol>
       <li><em>The Promised Land</em> -- try the <a target="_blank" rel="noreferrer" href="http://playthepromisedland.com/">development build</a> today.</li>
-      <li><em>Tower Story</em> -- a 3D Tower builder in the spirit of S*mT*w*r</li>
+      <li><em>Tower Story</em> -- a 3D Tower builder in the spirit of S*mT*w*r with S*ms like elements.</li>
       <li><em>From the Second Person: The Piano Man</em> -- a robo detective story told from the perspective of the second... person?</li>
       <li><em>Voodoo Trolluhnomics</em> -- what happens when everyone's a data zombie in a cyber dystopia and you're a witch? You deicide!</li>
     </ol>
