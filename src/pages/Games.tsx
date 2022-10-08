2146cfe0fdb18cc9913a570a414f71099ed9b82f
Anand Mallik <ccanandmallik@gmail.com>
2022-10-08 00:20:58 -0400

    less commits would be more professional

--- src/pages/Games.tsx
+++ src/pages/Games.tsx
@@ -8,7 +8,7 @@ const Games: React.FC = () => (
       <li><em>The Promised Land</em> -- try the <a target="_blank" rel="noreferrer" href="http://playthepromisedland.com/">development build</a> today.</li>
       <li><em>Tower Story</em> -- a 3D Tower builder in the spirit of S*mT*w*r with S*ms like elements.</li>
       <li><em>From the Second Person: The Piano Man</em> -- a robo detective story told from the perspective of the second... person?</li>
-      <li><em>Voodoo Trolluhnomics</em> -- what happens when everyone's a data zombie in a cyber dystopia and you're a witch? You deicide!</li>
+      <li><em>Voodoo Trolluhnomics</em> -- what happens when everyone else is a data zombie in a cyber dystopia and you're a witch with an analog vibe? You deicide!</li>
     </ol>
   </Container>
 )
